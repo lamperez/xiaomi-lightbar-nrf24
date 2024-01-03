d96090606698a20a53fd3db92d1a715368adc419
lamperez <dr.lamperez@gmail.com>
2024-01-03 11:42:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(name='xiaomi_lightbar_nrf24',
       version='0.1',
       description='Control de Xiaomi Monitor Light Bar with a nRF24 module',
-      # url='http://github.com/storborg/funniest',
+      url='https://github.com/lamperez/xiaomi-lightbar-nrf24',
       author='Alejandro García Lampérez',
       author_email='dr.lamperez@gmail.com',
       license='GPLv3',
-      # packages=['xiaomi_lightbar_nrf24'],
+      packages=['xiaomi_lightbar_nrf24'],
       install_requires=[
           'pyrf24',
           'crc',
