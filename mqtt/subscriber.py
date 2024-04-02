e45f51f36ec2651969c13ca4c07bb634c95474bf
ersonp <ersonpereiracr7@gmail.com>
2024-04-02 22:22:04 +0530

    feat: add args

--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -1,15 +1,49 @@
 import paho.mqtt.client as mqtt
 from xiaomi_lightbar import Lightbar
+import argparse
+
+description = """
+    MQTT subscriber for Xiaomi Lightbar Home Assistant MQTT Light integration.
+    This script subscribes to the MQTT topic and controls the Xiaomi Lightbar based on the received messages by using the xiaomi_lightbar library.
+"""
+
+parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+parser.add_argument("--broker", type=str, default="homeassistant.local", help="MQTT Broker")
+parser.add_argument("--port", type=int, default=1883, help="MQTT Port")
+parser.add_argument("--username", type=str, default="", help="MQTT Username")
+parser.add_argument("--password", type=str, default="", help="MQTT Password")
+parser.add_argument("--topic", type=str, default="xiaomi/lightbar", help="MQTT Topic")
+parser.add_argument("--ce_pin", type=int, default=25, help="CE Pin")
+parser.add_argument("--csn_pin", type=int, default=0, help="CSN Pin")
+parser.add_argument("--remote_id", type=lambda x: int(x, 16), default=0xABCDEF, help="Remote ID")
+
+args = parser.parse_args()
+
+CE_PIN = args.ce_pin
+CSN_PIN = args.csn_pin
+REMOTE_ID = args.remote_id
+BROKER = args.broker
+PORT = args.port
+USERNAME = args.username
+PASSWORD = args.password
+TOPIC = args.topic
+
+# Create Lightbar and MqttController instances
+lightbar = Lightbar(ce_pin=CE_PIN, csn_pin=CSN_PIN, remote_id=REMOTE_ID)
 
 class MqttController:
     def __init__(self, broker, port, username, password, topic, lightbar):
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-        self.client.username_pw_set(username, password)
+        if username != "":
+            self.client.username_pw_set(username, password)
         self.broker = broker
         self.port = port
         self.topic = topic + "/#"
         self.lightbar = lightbar
-        self.previous_control_state = ""
+        # Store the previous control state to avoid sending the same on_off command multiple times
+        # we assume the default state to be ON
+        self.previous_control_state = "ON"
 
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
@@ -24,7 +58,7 @@ class MqttController:
             print("Connected to MQTT Broker!")
             client.subscribe(self.topic)
         else:
-            print("Failed to connect, return code %d\n", rc)
+            print(f"Failed to connect, return code: {rc}")
             self.stop()
 
     def on_message(self, client, userdata, msg):
@@ -74,23 +108,14 @@ def scale_value(t):
         f_t = None
     return round(f_t) if f_t is not None else None
 
-# Constants for magic numbers and strings
-CE_PIN = 25
-CSN_PIN = 0
-REMOTE_ID = 0xa2c231
-BROKER = "homeassistant.local"
-PORT = 1883
-USERNAME = "mqtt_user"
-PASSWORD = "password"
-TOPIC = "xiaomi/lightbar"
+def main():
+    try:
+        with MqttController(BROKER, PORT, USERNAME, PASSWORD, TOPIC, lightbar) as controller:
+            controller.start()
+            while True:  # Keep the program running
+                pass
+    except KeyboardInterrupt:
+        print("\nInterrupted by user. Exiting...")
 
-# Create Lightbar and MqttController instances
-lightbar = Lightbar(ce_pin=CE_PIN, csn_pin=CSN_PIN, remote_id=REMOTE_ID)
-
-try:
-    with MqttController(BROKER, PORT, USERNAME, PASSWORD, TOPIC, lightbar) as controller:
-        controller.start()
-        while True:  # Keep the program running
-            pass
-except KeyboardInterrupt:
-    print("Interrupted by user. Exiting...")
+if __name__ == "__main__":
+    main()
